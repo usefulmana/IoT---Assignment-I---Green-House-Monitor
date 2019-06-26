25179e0da237cfd95b9fd190cd9fccc8fb2ea55e
Janie <emerald1901@gmail.com>
2019-06-26 14:59:31 +0700

    Seaborn completed

--- analytics.py
+++ analytics.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-import sqlite3
 import json
 
 
@@ -14,41 +13,83 @@ class DataVisualiser:
         with open('config.json') as config_file:
             self._config = json.load(config_file)
 
+    def visualise_report(self, using_plt=True):
+        # Using Seaborn style if specified
+        if not using_plt:
+            sns.set()
+
+        # Create figure with 2 subplots
+        fig, (ax_temp, ax_hum) = plt.subplots(1, 2, figsize=(12, 6))
+
+        # Attributes for each axes including titles and labels
+        ax_temp.set_title('Temperature')
+        ax_hum.set_title('Humidity')
+
+        # Option to use Matplotlib or Seaborn library
+        if using_plt:
+            self.plot_pyplot(fig=fig, ax_temp=ax_temp, ax_hum=ax_hum)
+        else:
+            sns.set()
+            self.plot_seaborn(fig=fig, ax_temp=ax_temp, ax_hum=ax_hum)
+
+        plt.show()
+
     # ==================================================================================
     # Visualisation with Matplotlib
     # ==================================================================================
 
-    def plot_pyplot(self):
-        # Create figure with 2 subplots
-        fig, (ax_temp, ax_hum) = plt.subplots(1, 2, figsize=(12, 6))
-
+    def plot_pyplot(self, fig, ax_temp, ax_hum):
         # Red line chart for temperature
-        ax_temp.set_title('Temperature')
-        ax_temp.set_ylabel('Degree C')
         ax_temp.plot(self._df['temperature'], color='red', label='Recorded temperature')
+        ax_temp.set_ylabel('Degree C')
         self.draw_constant_line(ax=ax_temp, mode='temperature')
 
         # Blue line chart for humidity
         ax_hum.plot(self._df['humidity'], color='blue')
-        ax_hum.set_title('Humidity')
         ax_hum.set_ylabel('%')
         self.draw_constant_line(ax=ax_hum, mode='humidity')
 
-        # Tight layout to avoid overlaps
-        plt.tight_layout()
-        fig.suptitle('Data Visualisation with Matplotlib', y=1.12)
+        # Set figure's title
+        fig.suptitle('Data Visualisation with Matplotlib', fontsize=20)
+
         # Save figure as PNG image file
         plt.savefig('visualise_matplotlib.png')
-        plt.show()
+
+    # ==================================================================================
+    # Visualisation with Seaborn
+    # ==================================================================================
+
+    def plot_seaborn(self, fig, ax_temp, ax_hum):
+        # Red line chart for temperature
+        sns.lineplot(x='index', y='temperature', color='red', label='Temperature',
+                     data=self._df.reset_index(), ax=ax_temp)
+        ax_temp.set_ylabel('Degree C')
+        self.draw_constant_line(ax=ax_temp, mode='temperature')
+
+        # Blue line chart for humidity
+        sns.lineplot(x='index', y='humidity', color='blue', label='Humidity',
+                     data=self._df.reset_index(), ax=ax_hum)
+        ax_hum.set_ylabel('%')
+        self.draw_constant_line(ax=ax_hum, mode='humidity')
+
+        # Set figure's title
+        fig.suptitle('Data Visualisation with Seaborn', fontsize=20)
+
+        # Save figure as PNG image file
+        plt.savefig('visualise_seaborn.png')
+
+    # ==================================================================================
+    # Visualisation with Seaborn
+    # ==================================================================================
 
     def draw_constant_line(self, ax, mode):
         ax.axhline(y=self._config['max_{}'.format(mode)], color='green', linewidth=1,
                    linestyle='dashed', label='Upper threshold')
-        ax.axhline(y=self._config['min_{}'.format(mode)], color='green', linewidth=1,
+        ax.axhline(y=self._config['min_{}'.format(mode)], color='olive', linewidth=1,
                    linestyle='dashed', label='Lower threshold')
         ax.legend()
         ax.get_xaxis().set_visible(False)
 
 
 data_visualiser = DataVisualiser()
-data_visualiser.plot_pyplot()
+data_visualiser.visualise_report(using_plt=True)
